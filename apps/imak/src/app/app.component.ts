084d69560a52144188879a5a81f49e091419f512
Avner Hattab <avner.h@nanox.vision>
2023-03-30 18:24:55 +0300

    Initial commit

---
+++ apps/imak/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'imakolet-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'imak';
+}
